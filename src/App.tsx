89258c0a6a911b83b051735d14812fbc537740be
StrikerXx798 <str1k4@yandex.ru>
2020-10-24 10:59:22 +0300

    First commit and first view

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss';
+import iPadHor from './assets/ipad-mini-horizontal_2.png'
+import iPadVer from './assets/ipad-mini-vertifical_2.png'
+import iPhoneHor from './assets/iphone5-horizontal_2.png'
+import iPhoneVer from './assets/iphone5-vertical_2.png'
+import background from './assets/background.jpg'
+import icon from './assets/icons/iTunes Store.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={s.app}>
+        <div className={s.device_wrapper}>
+            <picture>
+                <source srcSet={iPadHor} media='(min-width: 1024px)'/>
+                <source srcSet={iPadVer} media='(min-width: 768px)'/>
+                <source srcSet={iPhoneHor} media='(min-width: 568px)'/>
+                <img src={iPhoneVer} alt="" className={s.device}/>
+            </picture>
+            <div className={s.screen_wrapper}>
+                <img src={background} alt=""/>
+                <div className={s.screen}>
+                    <div className={s.top_bar}>
+                        Icons
+                    </div>
+                    <div  className={s.icons_set}>
+                        <div className={s.icons_wrapper}>
+                            <img src={icon} alt="" className={s.icons}/>
+                            <div className={s.app_name}>Music</div>
+                        </div>
+                        <div className={s.icons_wrapper}>
+                            <img src={icon} alt="" className={s.icons}/>
+                            <div className={s.app_name}>Music</div>
+                        </div>
+                        <div className={s.icons_wrapper}>
+                            <img src={icon} alt="" className={s.icons}/>
+                            <div className={s.app_name}>Music</div>
+                        </div>
+                        <div className={s.icons_wrapper}>
+                            <img src={icon} alt="" className={s.icons}/>
+                            <div className={s.app_name}>Music</div>
+                        </div>
+                    </div>
+                    <div className={s.swap_menu}></div>
+                    <div className={s.low_bar}></div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
